206d137b8a772df7b456072f99a755fdd5c54d87
Phu Mai <maiphu7777@gmail.com>
2026-06-15 14:45:47 +0700

    switch to static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
   integrations: [
     mdx(),
     sitemap(),
